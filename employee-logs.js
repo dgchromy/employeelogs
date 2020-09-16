0403ab0471f7e0493c3e9481e16c4fe7bc9cd4e3
Donald Chromy <dgchromy@gmail.comglobal user.emailgit config user.emailgit config --global user.email dgchromy@gmail.com>
2020-09-15 18:20:37 -0700

    added connection properties for mysql to work with employee-logs.js

--- employee-logs.js
+++ employee-logs.js
@@ -2,3 +2,11 @@ const mysql = require('mysql');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 const promisemysql = require('promise-mysql');
+
+const connectionProperties = {
+    host: 'localhost',
+    port: 4200,
+    user: 'root',
+    password: 'sunshine6992!',
+    database: 'employee_db' 
+}
