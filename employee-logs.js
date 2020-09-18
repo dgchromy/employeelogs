7d2e3b4803c408a31b81308a92f2e3672265cca0
Donald Chromy <dgchromy@gmail.comglobal user.emailgit config user.emailgit config --global user.email dgchromy@gmail.com>
2020-09-17 18:44:46 -0700

    added function to add role view employee by role

--- employee-logs.js
+++ employee-logs.js
@@ -151,4 +151,45 @@ function viewAllEmpByDept() {
         });
     });
 
+    //employee by role//
+
+function viewAllEmpByRole(){
+    let roleArr = [];
+
+    promisemysql.createConnection(connectionProperties)
+    .then((conn) => {
+        return conn.query('SELECT title FROM role');
+    }).then(function(roles){
+        for (i=0; i < roles.length; i++){
+            roleArr.push(roles[i].title);
+        }
+         }).then(() => {
+
+      //user to select role//    
+
+        inquirer.prompt({
+            name: 'role',
+            type: 'list',
+            message: 'Which role would you like to search?',
+            choices: roleArr
+        })
+        .then((answer) => {
+
+
+            //query for all employees by role selected by user//
+
+            const query = `SELECT e.id AS ID, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS Title, department.name AS Department, role.salary AS Salary, concat(m.first_name, ' ' ,  m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE role.title = '${answer.role}' ORDER BY ID ASC`;
+            connection.query(query, (err, res) => {
+                if(err) return err;
+
+                console.log('\n');
+                console.table(res);
+                mainMenu();
+            
+            });
+        });
+    });
+
+ }
+
 }
