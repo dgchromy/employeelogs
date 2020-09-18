6cf9dee3682eba4787e1d22ff341232984671f29
Donald Chromy <dgchromy@gmail.comglobal user.emailgit config user.emailgit config --global user.email dgchromy@gmail.com>
2020-09-17 17:58:25 -0700

    added main paige function

--- employee-logs.js
+++ employee-logs.js
@@ -23,12 +23,83 @@ inquirer
     .prompt({
         name: "action",
         type: 'list',
-        message: 'Main menu'
+        message: 'Main menu',
         choices: [
             'View all employee',
             'View employees by role',
             'View department',
-            'View all manager'
+            'View all manager',
+            'Add Employee',
+            'Add Role',
+            'Add Department',
+            'Update employee role',
+            'Update employee manager',
+            'Delete employee',
+            'Delete role',
+            'Delete department',
+            'View department budgets'
+
         ]
     })
+    .then((answer) => {
+        switch (answer.action) {
+            case 'View all employee':
+                viewAllEmp();
+                break;
+
+            case 'View all employees by department':
+                viewAllEmpByDept();
+                break;
+
+            case 'View all employees by role':
+                    viewAllEmpByRole();
+                    break;
+                
+            case 'Add Employee':
+                    addEmp();
+                    break;  
+
+            case 'Add Department':
+                    addDept();
+                    break; 
+
+             case 'add role':
+                    addRole();
+                    break;
+
+            case 'Update employee Role':
+                   updateEmpRole();
+                    break;
+
+            case 'Update employee manager':
+                    updateEmpMngr();
+                    break;
+
+            case 'view all employee manager':
+                    viewAllEmpByMngr();
+                    break;
+
+             case 'Delete employee':
+                    deleteEmp();
+                    break;
+
+            case 'View department budgets':
+                    viewDeptBudget();
+                    break;
+
+            case 'Delete role':
+                    deleteRole();
+                    break;
+
+            case 'Delete department':
+                    deleteDept();
+                    break;
+            
+        }
+    });
+}
+//Employee view function//
+
+function viewAllEmp(){
+    let query = 'SELECT * FROM employee '
 }
