91056a2cc1c7dbb8c73d1deb56a369b026c9ec39
Donald Chromy <dgchromy@gmail.comglobal user.emailgit config user.emailgit config --global user.email dgchromy@gmail.com>
2020-09-23 14:53:23 -0700

    added update manager role function

--- employee-logs.js
+++ employee-logs.js
@@ -413,3 +413,68 @@ function updateEmpRole(){
 
 }
 
+function updateEmpMngr(){
+
+    
+    let employeeArr = [];
+
+    
+    promisemysql.createConnection(connectionProperties
+    ).then((conn) => {
+
+        
+        return conn.query("SELECT employee.id, concat(employee.first_name, ' ' ,  employee.last_name) AS Employee FROM employee ORDER BY Employee ASC");
+    }).then((employees) => {
+
+        
+        for (i=0; i < employees.length; i++){
+            employeeArr.push(employees[i].Employee);
+        }
+
+        return employees;
+    }).then((employees) => {
+
+        inquirer.prompt([
+            {
+              
+                name: "employee",
+                type: "list",
+                message: "Who would you like to edit?",
+                choices: employeeArr
+            }, {
+               
+                name: "manager",
+                type: "list",
+                message: "Who is their new Manager?",
+                choices: employeeArr
+            },]).then((answer) => {
+
+                let employeeID;
+                let managerID;
+
+                
+                for (i=0; i < employees.length; i++){
+                    if (answer.manager == employees[i].Employee){
+                        managerID = employees[i].id;
+                    }
+                }
+
+                
+                for (i=0; i < employees.length; i++){
+                    if (answer.employee == employees[i].Employee){
+                        employeeID = employees[i].id;
+                    }
+                }
+
+                connection.query(`UPDATE employee SET manager_id = ${managerID} WHERE id = ${employeeID}`, (err, res) => {
+                    if(err) return err;
+
+                   
+                    console.log(`\n ${answer.employee} MANAGER UPDATED TO ${answer.manager}...\n`);
+
+                   
+                    mainMenu();
+                });
+            });
+    });
+}
