940ef9107fa6be970d2fe23d8b707e9773f8f8a1
Donald Chromy <dgchromy@gmail.comglobal user.emailgit config user.emailgit config --global user.email dgchromy@gmail.com>
2020-10-20 16:52:10 -0700

    adding readme and gif for screenshot showing the app works

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # employeelogs
+
+
+    # Project Title : employeelogs
+  ## Project Description:
+  it is a program that generates charts to look at employees for a business threw the terminal you can add departments, employees, roles 
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Test](#test)
+  * [Questions](#questions)
+  * [License](#license)
+  * [Author] (#Author)
+  * [Badges](#badges)
+  ## Installation
+  npm i to install necessary node packages require mysql 
+  ## Usage
+  run terminal node server.js
+  ## Contributors
+  doug mitchell and jonathon bower 
+  ## Test
+  run terminal node server.js use arrow to select a branch 
+  ## Questions
+  If you have any questions, contact dgchromy on GitHub.
+  ## License
+  MIT License https://img.shields.io/github/license/dgchromy/ReadMeGenerator
+  Copyright (c) [year] [dgchromy]
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+  ## Author
+  ![GitHub profile pic](https://avatars3.githubusercontent.com/u/65515339?v=4)
+  ## Badges
+
+
+
+
--- server.js
+++ server.js
@@ -3,197 +3,232 @@ const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Sunshine6992!',
-    database: 'employees_DB' 
-});
+const connection = mysql.createConnection(
+    {
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'Sunshine6992!',
+        database: 'employees_DB'
+    }
+);
 
 
-connection.connect(function (err){
-    if (err) throw err; 
+connection.connect(function (err)
+{
+    if (err)
+    {
+        throw err;
+    }
     console.log('\n The Employee Tracker.. \n');
-    searchDataBase()
+    searchDataBase();
 });
 
-function searchDataBase() {
-    inquirer
-      .prompt({
-        name: "create",
-        type: "list",
-        message: "Create a [Department], a [Role], an [Employee] or [View] the following:",
-              choices: ["Department", "Role", "Employee", "View"]
-          })
-          .then(function(promptOptions){
-              if (promptOptions.create === "Department") {
-                  addDepartment();
-              }
-              else if(promptOptions.create === "Role") {
-                  addRole();
-              }
-              else if(promptOptions.create === "Employee") {
-                  addEmployee();
-              }
-              else if(promptOptions.create === "View") {
-                  viewData();
-              }
-              else{
-                  connection.end();
-              }
-          })
-  }
-//creating function to add Department // 
-
-  function addDepartment() {
-    inquirer
-      .prompt([
+function searchDataBase()
+{
+    inquirer.prompt(
         {
-          name: "department",
-          type: "input",
-          message: "What department do you want to add?",
+            name: "create",
+            type: "list",
+            message: "Create a [Department], a [Role], an [Employee] or [View] the following:",
+            choices: ["Department", "Role", "Employee", "View"]
         }
-      ])
-      .then(function(answer) {
-        connection.query(
-          "INSERT INTO department SET ?",
-          {
+    ).then(function (promptOptions)
+    {
+        if (promptOptions.create === "Department")
+        {
+            addDepartment();
+        }
+        else if (promptOptions.create === "Role")
+        {
+            addRole();
+        }
+        else if (promptOptions.create === "Employee")
+        {
+            addEmployee();
+        }
+        else if (promptOptions.create === "View")
+        {
+            viewData();
+        }
+        else
+        {
+            connection.end();
+        }
+    });
+}
+// creating function to add Department //
+
+function addDepartment()
+{
+    inquirer.prompt([{
+            name: "department",
+            type: "input",
+            message: "What department do you want to add?"
+        }]).then(function (answer)
+    {
+        connection.query("INSERT INTO department SET ?",
+        {
             name: answer.department
-          },
-          function(err) {
-            if (err) throw err;
+        }, function (err)
+        {
+            if (err)
+            {
+                throw err;
+            }
             console.log("Your department was added!");
             searchDataBase();
-          }
-        );
-      });
-  }
-//add role function// 
-  function addRole() {
-    inquirer
-      .prompt([
+        });
+    });
+}
+// add role function//
+function addRole()
+{
+    inquirer.prompt([
         {
-          name: "title",
-          type: "input",
-          message: "What role title are you adding?",
+            name: "title",
+            type: "input",
+            message: "What role title are you adding?"
         },
         {
-          name: "salary",
-          type: "input",
-          message: "The salary for this role?",
+            name: "salary",
+            type: "input",
+            message: "The salary for this role?"
         },
         {
-          name: "departmentId",
-          type: "input",
-          message: "What departmentID are we adding?",
+            name: "departmentId",
+            type: "input",
+            message: "What departmentID are we adding?"
         }
-      ])
-      .then(function(answer) {
+    ]).then(function (answer)
+    {
 
-        connection.query(
-          "INSERT INTO role SET ?",
-          {
+        connection.query("INSERT INTO role SET ?",
+        {
             title: answer.title,
             salary: answer.salary,
             department_id: answer.departmentId
-          },
-          function(err) {
-            if (err) throw err;
+        }, function (err)
+        {
+            if (err)
+            {
+                throw err;
+            }
             console.log("Role Added!");
             searchDataBase();
-          }
-        );
-      });
-  }
+        });
+    });
+}
 
-  function addEmployee() {
-    inquirer
-      .prompt([
+function addEmployee()
+{
+    inquirer.prompt([
         {
-          name: "first",
-          type: "input",
-          message: "First name of the new Employee",
+            name: "first",
+            type: "input",
+            message: "First name of the new Employee"
         },
         {
-          name: "last",
-          type: "input",
-          message: "last name of new employee?",
+            name: "last",
+            type: "input",
+            message: "last name of new employee?"
         },
         {
-          name: "roleId",
-          type: "input",
-          message: "What is role does new employee have?"
+            name: "roleId",
+            type: "input",
+            message: "What is role does new employee have?"
         },
         {
-          name: "managerId",
-          type: "input",
-          message: "what is the manager id if employee is a manager",
+            name: "managerId",
+            type: "input",
+            message: "what is the manager id if employee is a manager"
         }
-      ])
-      .then(function(answer) {
-        connection.query(
-          "INSERT INTO employee SET ?",
-          {
+    ]).then(function (answer)
+    {
+        connection.query("INSERT INTO employee SET ?",
+        {
             first_name: answer.first,
             last_name: answer.last,
             role: answer.roleId,
             manager_id: answer.managerId
-          },
-          function(err) {
-            if (err) throw err;
+        }, function (err)
+        {
+            if (err)
+            {
+                throw err;
+            }
             console.log("Employee Added!");
             searchDataBase();
-          }
-        );
-      });
-  }
+        });
+    });
+}
 
 
-  function viewData() {
-    inquirer
-      .prompt({
-        name: "table",
-        type: "list",
-        message: "what would you want to look up?",
-        choices: ["Department", "Role", "Employee"]
-      })
-      .then(function(answer) {
-        if (answer.table === "Department") {
-          viewDepartment();
+function viewData()
+{
+    inquirer.prompt(
+        {
+            name: "table",
+            type: "list",
+            message: "what would you want to look up?",
+            choices: ["Department", "Role", "Employee"]
         }
-        else if(answer.table === "Role") {
-          viewRole();
-        } 
-        else if(answer.table === "Employee") {
-          viewEmployee();
+    ).then(function (answer)
+    {
+        if (answer.table === "Department")
+        {
+            viewDepartment();
         }
-        else{
-          connection.end();
+        else if (answer.table === "Role")
+        {
+            viewRole();
         }
-      });
-  }
+        else if (answer.table === "Employee")
+        {
+            viewEmployee();
+        }
+        else
+        {
+            connection.end();
+        }
+    });
+}
 
-  function viewDepartment() {
-    connection.query("SELECT * FROM department", function(err,res){
-      if (err) throw err;
-      console.table(res);
-      searchDataBase();
-    })
-  };
+function viewDepartment()
+{
+    connection.query("SELECT * FROM department", function (err, res)
+    {
+        if (err)
+        {
+            throw err;
+        }
+        console.table(res);
+        searchDataBase();
+    });
+};
 
-  function viewRole() {
-    connection.query("SELECT * FROM role", function(err,res){
-      if (err) throw err;
-      console.table(res);
-      searchDataBase();
-        })
-    };
+function viewRole()
+{
+    connection.query("SELECT * FROM role", function (err, res)
+    {
+        if (err)
+        {
+            throw err;
+        }
+        console.table(res);
+        searchDataBase();
+    });
+};
 
-    function viewEmployee() {
-        connection.query("SELECT * FROM employee", function(err,res){
-          if (err) throw err;
-          console.table(res);
-          searchDataBase();
-        })
-      };
+function viewEmployee()
+{
+    connection.query("SELECT * FROM employee", function (err, res)
+    {
+        if (err)
+        {
+            throw err;
+        }
+        console.table(res);
+        searchDataBase();
+    });
+};
